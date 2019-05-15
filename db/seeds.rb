49fe203d95db7730f04ff178489fa69ebb257fbd
Iris Wolfisberg <iris.wolfisberg@hotmail.fr>
2019-05-15 12:23:50 +0200

    Made seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Flat.create([
+    {
+      name: Faker::Lorem.sentence,
+      address: Faker::Address.street_address,
+      description: Faker::Lorem.paragraph_by_chars,
+      price_per_night: Faker::Number.number(2),
+      number_of_guests: Faker::Number.digit
+    }
+  ])
+end
